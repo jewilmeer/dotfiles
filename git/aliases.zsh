7025451bf14b655a4f8e68c9bbc9495565d0b132
Jan-Willem van der Meer <mail@jewilmeer.nl>
2013-05-23 10:07:27 +0200

    Add new aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -17,3 +17,5 @@ alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+alias ll='ls -lrthG'
+alias be='bundle exec'
